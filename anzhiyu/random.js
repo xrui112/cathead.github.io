5f9e62fe16bde9325db9c36039b1600c5899437e
caathead <155051876@qq.com>
2024-12-18 22:32:28 +0800

    Site updated: 2024-12-18 22:32:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/17/hello-world/","2024/12/17/PostPages/"];function toRandomPost(){
+var posts=["2024/12/17/PostPages/","2024/12/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
