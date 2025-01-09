c6dab4fe06f9c516d476867fa424ea0f85662823
caathead <155051876@qq.com>
2025-01-09 15:31:13 +0800

    Site updated: 2025-01-09 15:31:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/23/OS/","2024/12/25/OSTest/","2024/12/17/PostPages/","2024/12/31/计算机网络/"];function toRandomPost(){
+var posts=["2024/12/25/OSTest/","2024/12/23/OS/","2024/12/17/PostPages/","2024/12/31/计算机网络/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
